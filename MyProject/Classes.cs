b3f18e8d29148501acdd26e1eb50921ecdc1be09
soniaa28 <sofija.demchuk@gmail.com>
2023-03-20 22:53:46 +0200

    Add project files.

---
+++ MyProject/Classes.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MyProject
+{
+    class Bell
+    {
+        public delegate void BellHandler(string msgBell);
+        public event BellHandler Lesson;
+        public event BellHandler Break;
+
+
+        private void onLesson(string msg)
+        {
+
+            Lesson?.Invoke(msg);
+        }
+        private void onBreak(string msg)
+        {
+
+            Break?.Invoke(msg);
+        }
+
+
+    }
+    class Person
+    {
+        public string Name { get; private set; }
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+        public override string ToString()
+        {
+            return this.GetType().Name + ' ' + Name;
+        }
+    }
+    class Student : Person
+    {
+        public int knowledge;
+        public Student(string name, int _knowledge) : base(name) 
+        {
+            this.knowledge = knowledge;
+        }
+
+        public int Knowledge
+        {
+            get { return this.knowledge; }
+            private set { this.knowledge = value; }
+        }
+
+        public void Study() { }
+
+    }
+
+    class Teacher : Person
+    {
+        public Teacher(string name) : base(name) { }
+
+        public void Teach() { }
+        public void setGradeToStudents() { }
+    }
+
+}
---
+++ MyProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyProject;
+using System;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+class Program
+{
+    static void Main()
+    {
+        Bell bell = new Bell();                                                                    
+        // Create a timer object
+        Timer timer = new Timer();
+        timer.Interval = TimeSpan.FromSeconds(5).TotalMilliseconds; // Change day to night every 24 hours
+        timer.Elapsed += Bell.Lesson;
+        timer.Start();
+
+        // Wait for user input to exit the program
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+
+  
+}
