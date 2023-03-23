9ed81167aeb6e153a7a6e6b9569a06bc687b50f1
soniaa28 <sofija.demchuk@gmail.com>
2023-03-23 23:07:02 +0200

    final

--- MyProject/Classes.cs
+++ MyProject/Classes.cs
@@ -18,11 +18,8 @@ namespace MyProject
         public event BellEventHandler Lesson;
         public event BellEventHandler Break;
 
-
-
         public void StartLesson() //method for сalling the event Lesson
         {
-          
             OnLesson();
         }
 
@@ -54,17 +51,15 @@ namespace MyProject
     }
     class Student : Person //class Student know how to react on events , has own event MaxNote to inform that he already passed the course and shoud quit
     {
+        //annonce delegate and event
+        public delegate void AmountEvent(object sender, EventArgs arg);
+        public event AmountEvent MaxNote;
 
         public int knowledge; //field for saving the current note
-
-        //annonce delegate and event
-        public delegate void AmountEvent(object sender, EventArgs arg); 
-        public event AmountEvent MaxNote;
         public Student(string name, int _knowledge) : base(name) // using constructor of Person to implement this constructor  
         {
-            this.knowledge = knowledge;
+            this.knowledge = _knowledge;
         }
-      
         //property
         public int Knowledge
         {
@@ -79,8 +74,6 @@ namespace MyProject
                 {
                     this.knowledge = value;
                 }
-               
-
             }
         }
         //dispetcher
@@ -88,60 +81,50 @@ namespace MyProject
         {
             MaxNote?.Invoke(this, EventArgs.Empty);
         }
-
     }
-
     class GroupOfStudents : IEnumerable
     {
-        public delegate void PresentEventHandler(object sender, EventArgs e);
+        public delegate void PresentEventHandler(object sender, EventArgs e); 
 
-        public event PresentEventHandler Present;
+        public event PresentEventHandler Present;//event to inform Teacher that Students are present in a class
 
-        static string[] NAMES = { "Андрій", "Світлана", "Оксана", "Олена", "Остап", "Маркіян", "Генадій", "Олексій", "Данило" };
+        static string[] NAMES = { "Андрій", "Світлана", "Оксана", "Олена", "Остап", "Маркіян", "Генадій", "Олексій", "Данило" }; // array of names for new students
 
-
-
-        List<Student> students;
+        List<Student> students; //list of students
      
         public GroupOfStudents(List<Student> students)
         {
             this.students = students;
             
         }
-        
-
-        public Student this[int index]
+        public Student this[int index]  //indexator for accesing the student from list 
         {
             get => students[index];
             set=>students[index] = value;
         }
-        public int CountGroup
+        public int CountGroup // property to have count of students for looping
         {
             get
             {
                 return students.Count;
             }
         }
+        //dispetcher
         public void IsPresent()
         {
             Present?.Invoke(this, EventArgs.Empty);
         }
-        public void GoToLesson(object sender, EventArgs e)
+        public void GoToLesson(object sender, EventArgs e) ////method if the lesson event occur
         {
-
             foreach (Student student in students)
             {
                 Console.WriteLine(student.Name + " на уроці.");
 
             }
             IsPresent();
-          
-
         }
-
-        public void GoToBreak(string msg)
+        public void GoToBreak(string msg) // method if the FinishLesson event of Teacher occur
         {
-
             foreach (Student student in students)
             {
                 Console.WriteLine(student.Name + " іде відпочивати на перерву.");
@@ -149,12 +132,12 @@ namespace MyProject
             }
         }
 
-        public IEnumerator GetEnumerator()
+        public IEnumerator GetEnumerator() // realised interface IEnumerable for enumerating the students in loop
         {
             return ((IEnumerable)students).GetEnumerator();
         }
 
-        public void GraduateStudent(object sender, EventArgs e)
+        public void GraduateStudent(object sender, EventArgs e) // method if the event MaxNote occur
         {
             Student student = sender as Student;
             students.Remove(student);
@@ -162,7 +145,7 @@ namespace MyProject
             NewRandomStudent();
         }
 
-        public void NewRandomStudent()
+        public void NewRandomStudent() // methos for adding a new Student 
         {
             Random random = new Random();
             int name = random.Next(0, NAMES.Length);
@@ -173,33 +156,29 @@ namespace MyProject
     }
 
 
-        class Teacher : Person
+        class Teacher : Person //class Teacher, knows how to react on events of Bell, knows how to inform the group of students about the ending of lesson
         {
             public delegate void FinishEventHandler(string msg);
+            public event FinishEventHandler Finish; //event to inform about the end of the lesson
 
-            public event FinishEventHandler Finish;
             private Random random = new Random();
-            public Teacher(string name) : base(name) { }
+            public Teacher(string name) : base(name) { } // constructor 
 
-            public void Teach() { }
-            public int GetRandomNote()
+            public int GetRandomNote() // method generate a random note for setting it to Student
             {
                 return random.Next(1, 10);
             }
-            public void GoToLesson(object sender, EventArgs e)
+            public void GoToLesson(object sender, EventArgs e) // if Lesson event occur
             {
-
                 Console.WriteLine(Name + " іде на урок..");
-
             }
-            public void GoToBreak(object sender, EventArgs e)
+            public void GoToBreak(object sender, EventArgs e) // if Break event occur
             {
                 FinishTheLesson();
                 Console.WriteLine(Name + "  закінчив пояснювати матеріал та йде пити каву у свій кабінет.");
             }
-            public void setGradeToStudents(GroupOfStudents group)
+            public void setGradeToStudents(GroupOfStudents group) // method for setting a random note for each student
             {
-                
                 for(int i = 0; i < group.CountGroup; i++)
                 {
                     int temp_note = GetRandomNote();
@@ -207,21 +186,18 @@ namespace MyProject
                     Console.WriteLine(string.Format("{0} поставив оцінку {1} студенту {2} , тепер його поточна оцінка = {3}", this.Name, temp_note, group[i].Name, group[i].Knowledge));
                 }
             }
-            public void checkStudents(object sender, EventArgs e)
+            public void checkStudents(object sender, EventArgs e) // check the presense and set notes
             {
                 GroupOfStudents group = sender as GroupOfStudents;
                 setGradeToStudents(group);
-
             }
-
-            public void FinishTheLesson()
+             //dispetcher
+            public void FinishTheLesson() 
             {
                 Finish?.Invoke("Урок завершено!");
             }
-
-       
-
     }
 
-    }
+}
+
 
--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -8,23 +8,26 @@ class Program
     static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Bell bell = new Bell();
-
-        Student alice = new Student("Соф'я",0 );
+        Bell bell = new Bell(); //creating object Bell 
+        // creating Students
+        Student alice = new Student("Соф'я",0 ); 
         Student bob = new Student ("Олександр",0 );
         Student charlie = new Student ("Юлія",0 );
+       
         List<Student> groupa = new List<Student> { alice,bob, charlie };
-        GroupOfStudents groupA = new GroupOfStudents(groupa);
-        Teacher teacher = new Teacher("Сергій Адамович");
-        foreach(Student student in groupA)
+        GroupOfStudents groupA = new GroupOfStudents(groupa); //creating a group
+
+        Teacher teacher = new Teacher("Сергій Адамович");//creating a teacher 
+
+        foreach (Student student in groupA)
         {
-            student.MaxNote += groupA.GraduateStudent;
+            student.MaxNote += groupA.GraduateStudent; // for subsribing on event MaxNote
         }
-        bell.Lesson += teacher.GoToLesson;
-        bell.Lesson += groupA.GoToLesson;
-        groupA.Present += teacher.checkStudents;
-        bell.Break +=teacher.GoToBreak;
-        teacher.Finish += groupA.GoToBreak;
+        bell.Lesson += teacher.GoToLesson; // subscribe teacher on Bell event 
+        bell.Lesson += groupA.GoToLesson;// subscribe group on Bell event 
+        groupA.Present += teacher.checkStudents; // subscribe teacher on StudentsGroup event 
+        bell.Break +=teacher.GoToBreak;// subscribe teacher on Bell event 
+        teacher.Finish += groupA.GoToBreak;// subscribe group on teacher event  about ending the class
 
         for (int i = 0; i < 7; i++)
         {
